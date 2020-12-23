f5fbf7575fadcce1e8f66e7ab24037e7fb2bb21b
Ryan Leong <hello@ryanleong.net>
2020-12-23 18:46:24 +0800

    Set up TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./pages/**/*.js", "./components/**/*.js"], // Remove unused styles in production
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
