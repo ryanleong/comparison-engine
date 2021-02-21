ff511a6869efb0aa2c7d19d9dbab7b3b7e05a278
Ryan Leong <hello@ryanleong.net>
2021-02-21 13:38:00 +0800

    Set up GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 /* eslint-disable react/prop-types */
+import TagManager from 'react-gtm-module';
+import { useEffect } from 'react';
 import 'styles/globals.scss';
 import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: process.env.NEXT_PUBLIC_GTM_ID,
+    });
+  }, []);
+
   return (
     <div id="webapp" className="bg-white text-gray-800">
       <div className="h-2.5 w-full bg-accent"></div>
